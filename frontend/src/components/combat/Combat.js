aac41362cf14a6a0c851349c81fbc08001b200b4
victorbofill <victorm.bofill@gmail.com>
2018-07-24 16:54:07 -0700

    combat in a super rough draft state

--- frontend/src/components/combat/Combat.js
+++ frontend/src/components/combat/Combat.js
@@ -1,19 +1,90 @@
-import React, { PureComponent } from 'react';
-// import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import Combatant from './Combatant';
+import styles from './Combat.css';
 
-class Combat extends PureComponent {
+export default class Combat extends Component {
+
+  state = {
+    combatants: [],
+    dr: 0,
+    apAdjust: 0,
+    str: 0,
+    agi: 0,
+    end: 0,
+    will: 0,
+    cha: 0,
+    rea: 0,
+    per: 0
+  };
+
+  addCombatant = () => {
+    this.setState(({ combatants }) => {
+      combatants.push({
+        dr: this.state.dr,
+        apAdjust: this.state.apAdjust,
+        str: this.state.str,
+        agi: this.state.agi,
+        end: this.state.end,
+        will: this.state.will,
+        cha: this.state.cha,
+        rea: this.state.rea,
+        per: this.state.per
+      });
+      return combatants;
+    });
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.id] : target.value });
+  };
+
   render() {
     return (
-      <div>
-        <main>
-          <h1>Combat</h1>
-          <p>This is the combat section.</p>
-        </main>
+      <div className={styles.combat}>
+        <h1>Combat</h1>
+        <div className="left">
+          <form className={styles.combatantForm}>
+            <label>DR: {this.state.dr}</label>
+            <input defaultValue="0" id="dr" onChange={this.handleChange} type="range" name="DR" min="0" max="20" />
+
+            <label>AP Adjust: {this.state.apAdjust}</label>
+            <input defaultValue="-1" id="apAdjust" onChange={this.handleChange} type="range" name="DR" min="-1" max="2" />
+
+            <label>STR: {this.state.str}</label>
+            <input defaultValue="1" id="str" onChange={this.handleChange} type="range" name="DR" min="1" max="4" />
+
+            <label>AGI: {this.state.agi}</label>
+            <input defaultValue="1" id="agi" onChange={this.handleChange} type="range" name="DR" min="1" max="4" />
+
+            <label>END: {this.state.end}</label>
+            <input defaultValue="1" id="end" onChange={this.handleChange} type="range" name="DR" min="1" max="4" />
+
+            <label>Will: {this.state.will}</label>
+            <input defaultValue="1" id="will" onChange={this.handleChange} type="range" name="DR" min="1" max="4" />
+
+            <label>CHA: {this.state.cha}</label>
+            <input defaultValue="1" id="cha" onChange={this.handleChange} type="range" name="DR" min="1" max="4" />
+
+            <label>REA: {this.state.rea}</label>
+            <input defaultValue="1" id="rea" onChange={this.handleChange} type="range" name="DR" min="1" max="4" />
+
+            <label>PER: {this.state.per}</label>
+            <input defaultValue="1" id="per" onChange={this.handleChange} type="range" name="DR" min="1" max="4" />
+
+          </form>
+          <button onClick={this.addCombatant}>ADD</button>
+        </div>
+        <div className={styles.right}>
+          <ul>
+            { this.state.combatants.map((combatant, i) => (
+              <Combatant
+                key={i}
+                combatant={combatant}
+                combatantIndex={i} />
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
 }
-
-export default connect(
-)(Combat);
--- frontend/src/components/combat/Combatant.js
+++ frontend/src/components/combat/Combatant.js
@@ -1,17 +1,46 @@
-import React, { PureComponent } from 'react';
-// import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-class Combatant extends PureComponent {
+export default class Combatant extends Component {
+  static propTypes = {
+    combatant: PropTypes.any,
+  };
+
   render() {
+    const { dr, apAdjust, str, agi, end, will, cha, rea, per } = this.props.combatant;
+
     return (
-      <div>
+      <li>
         <h3>Combatant</h3>
-        <p>This is a combatant.</p>
-      </div>
+        <table>
+          <thead>
+            <tr>
+              <th>DR</th>
+              <th>AP Adjust</th>
+              <th>STR</th>
+              <th>AGI</th>
+              <th>END</th>
+              <th>Will</th>
+              <th>CHA</th>
+              <th>REA</th>
+              <th>PER</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{dr}</td>
+              <td>{apAdjust}</td>
+              <td>{str}</td>
+              <td>{agi}</td>
+              <td>{end}</td>
+              <td>{will}</td>
+              <td>{cha}</td>
+              <td>{rea}</td>
+              <td>{per}</td>
+            </tr>
+          </tbody>
+        </table>
+      </li>
     );
   }
 }
-
-export default connect(
-)(Combatant);
