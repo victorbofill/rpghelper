b645ec2b757d9b1426317c6263eb94df744a8735
victorbofill <victorm.bofill@gmail.com>
2018-07-23 17:29:14 -0700

    notes and npcs routes set up

--- backend/lib/routes/npcs.js
+++ backend/lib/routes/npcs.js
@@ -1,29 +1,45 @@
 const router = require('express').Router();
-const Note = require('../models/Note');
+const NPC = require('../models/NPC');
 const { updateOptions } = require('../utils/mongoose-helpers');
 
 module.exports = router
   .get('/', (req, res, next) => {
-    Note.find()
+    NPC.find()
       .lean()
       .then(location => res.json(location))
       .catch(next);
   })
 
   .post('/', (req, res, next) => {
-    Note.create(req.body)
+    NPC.create(req.body)
       .then(location => res.json(location))
       .catch(next);
   })
 
   .put('/:id', (req, res, next) => {
-    return Note.findByIdAndUpdate(req.params.id, req.body, updateOptions)
+    const {
+      name,
+      disposition,
+      stats,
+      notes
+    } = req.body;
+
+    const update = {
+      name,
+      disposition,
+      stats,
+      notes
+    };
+
+    Object.keys(update).forEach(key => {if(!update[key]) delete update[key];});
+
+    return NPC.findByIdAndUpdate(req.params.id, update, updateOptions)
       .then(updated => res.json(updated))
       .catch(next);        
   })
-  
+
   .delete(':id', (req, res, next) => {
-    return Note.findByIdAndRemove(req.params.id)
+    return NPC.findByIdAndRemove(req.params.id)
       .then(() => res.json({ deleted: true }))
       .catch(next);
   });
