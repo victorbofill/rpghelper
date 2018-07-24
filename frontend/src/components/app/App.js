7825bff93be3dca1cb0ef94fb2ee9b1d473a0b74
victorbofill <victorm.bofill@gmail.com>
2018-07-23 22:08:52 -0700

    moved npcs and stories into locations

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -8,37 +8,31 @@ import Combat from '../combat/Combat';
 import Dice from '../dice/Dice';
 import Location from '../location/Location';
 import Notes from '../notes/Notes';
-import NPC from '../npc/NPC';
-import Story from '../story/Story';
 
 class App extends PureComponent {
   render() {
     return (
       <Router>
         <div>
-          <main className={styles.app}>
+          <header className={styles.header}>
+            <ul>
+              <li><NavLink to="/location">Location</NavLink></li>
+              <li><NavLink to="/combat">Combat</NavLink></li>
+              <li><NavLink to="/notes">Notes</NavLink></li>
+            </ul>
+          </header>
+ 
+          <main className={styles.main}>
             <div className={styles.dice}>
               <Dice />
             </div>
-
             <div>
-              <div className={styles.header}>
-                <ul>
-                  <li><NavLink to="/combat">Combat</NavLink></li>
-                  <li><NavLink to="/location">Location</NavLink></li>
-                  <li><NavLink to="/notes">Notes</NavLink></li>
-                  <li><NavLink to="/NPC">NPC</NavLink></li>
-                  <li><NavLink to="/story">Story</NavLink></li>
-                </ul>
-                <div className={styles.content}>
-                  <Switch>
-                    <Route path="/combat" component={Combat}/>
-                    <Route path="/location" component={Location}/>
-                    <Route path="/notes" component={Notes}/>
-                    <Route path="/NPC" component={NPC}/>
-                    <Route path="/story" component={Story}/>
-                  </Switch>
-                </div>
+              <div className={styles.content}>
+                <Switch>
+                  <Route path="/location" component={Location}/>
+                  <Route path="/combat" component={Combat}/>
+                  <Route path="/notes" component={Notes}/>
+                </Switch>
               </div>
             </div>
           </main>
