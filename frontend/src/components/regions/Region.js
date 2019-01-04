78c0439485189ca11bf2fe9a1e4eac4855778d01
victorbofill <victorm.bofill@gmail.com>
2019-01-04 15:22:54 -0800

    moved Region.js over

---
+++ frontend/src/components/regions/Region.js
@@ -0,0 +1,42 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import NPCs from './NPCs';
+import Locations from './Locations';
+import styles from './Regions.css';
+
+class Region extends PureComponent {
+  static propTypes = {
+    region: PropTypes.any
+  };
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <header className={styles.header}>
+            <ul>
+              <li><NavLink to="/NPCs">NPCs</NavLink></li>
+              <li><NavLink to="/locations">Locations</NavLink></li>
+            </ul>
+          </header>
+
+          <main>
+            <div>
+              <div>
+                <Switch>
+                  <Route path="/NPCs" component={NPCs}/>
+                  <Route path="/locations" component={Locations}/>
+                </Switch>
+              </div>
+            </div>
+          </main>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default connect(
+)(Region);
