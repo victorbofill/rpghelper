42aef3e659eda0075babdd2303880a2c860de7c2
victorbofill <victorm.bofill@gmail.com>
2019-01-11 13:02:56 -0800

    moved NPCs ctd.

---
+++ frontend/src/components/NPCs/actions.js
@@ -0,0 +1,41 @@
+import {
+  postNPC,
+  getNPCs,
+  putNPC,
+  delNPC
+} from '../../services/api';
+
+import {
+  NPC_POST,
+  NPCS_LOAD,
+  NPC_UPDATE,
+  NPC_REMOVE
+} from './reducers';
+
+export function addNPC() {
+  return {
+    type: NPC_POST,
+    payload: postNPC()
+  };
+}
+
+export function loadNPCs() {
+  return {
+    type: NPCS_LOAD,
+    payload: getNPCs()
+  };  
+}  
+
+export function updateNPC(region) {
+  return {
+    type: NPC_UPDATE,
+    payload: putNPC(region)
+  };
+}
+
+export function deleteNPC(id) {
+  return {
+    type: NPC_REMOVE,
+    payload: delNPC(id)
+  };
+}
