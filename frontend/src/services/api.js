57c4935e586bbe87ee751f50d95333f0acf80fc9
victorbofill <victorm.bofill@gmail.com>
2019-01-11 16:55:33 -0800

    updated api post rules

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -31,43 +31,43 @@ const REGIONS_URL = `${URL}/regions`;
 const STORIES_URL = `${URL}/stories`;
 const SUBREGIONS_URL = `${URL}/subregions`;
 
-export const postAsset = asset => post(`${ASSETS_URL}`, asset);
+export const postAsset = () => post(`${ASSETS_URL}`);
 export const getAssets = () => get(`${ASSETS_URL}`);
 export const getAsset = id => get(`${ASSETS_URL}/${id}`);
 export const putAsset = asset => put(`${ASSETS_URL}/${asset._id}`, asset);
 export const delAsset = id => del(`${ASSETS_URL}/${id}`);
 
-export const postBase = base => post(`${BASES_URL}`, base);
+export const postBase = () => post(`${BASES_URL}`);
 export const getBases = () => get(`${BASES_URL}`);
 export const getBase = id => get(`${BASES_URL}/${id}`);
 export const putBase = base => put(`${BASES_URL}/${base._id}`, base);
 export const delBase = id => del(`${BASES_URL}/${id}`);
 
-export const postChapter = chapter => post(`${CHAPTERS_URL}`, chapter);
+export const postChapter = () => post(`${CHAPTERS_URL}`);
 export const getChapters = () => get(`${CHAPTERS_URL}`);
 export const getChapter = id => get(`${CHAPTERS_URL}/${id}`);
 export const putChapter = chapter => put(`${CHAPTERS_URL}/${chapter._id}`, chapter);
 export const delChapter = id => del(`${CHAPTERS_URL}/${id}`);
 
-export const postEntry = entry => post(`${ENTRIES_URL}`, entry);
+export const postEntry = () => post(`${ENTRIES_URL}`);
 export const getEntries = () => get(`${ENTRIES_URL}`);
 export const getEntry = id => get(`${ENTRIES_URL}/${id}`);
 export const putEntry = entry => put(`${ENTRIES_URL}/${entry._id}`, entry);
 export const delEntry = id => del(`${ENTRIES_URL}/${id}`);
 
-export const postLocation = location => post(`${LOCATIONS_URL}`, location);
+export const postLocation = () => post(`${LOCATIONS_URL}`);
 export const getLocations = () => get(`${LOCATIONS_URL}`);
 export const getLocation = id => get(`${LOCATIONS_URL}/${id}`);
 export const putLocation = location => put(`${LOCATIONS_URL}/${location._id}`, location);
 export const delLocation = id => del(`${LOCATIONS_URL}/${id}`);
 
-export const postNote = note => post(`${NOTES_URL}`, note);
+export const postNote = () => post(`${NOTES_URL}`);
 export const getNotes = () => get(`${NOTES_URL}`);
 export const getNote = id => get(`${NOTES_URL}/${id}`);
 export const putNote = note => put(`${NOTES_URL}/${note._id}`, note);
 export const delNote = id => del(`${NOTES_URL}/${id}`);
 
-export const postNPC = NPC => post(`${NPCS_URL}`, NPC);
+export const postNPC = () => post(`${NPCS_URL}`);
 export const getNPCs = () => get(`${NPCS_URL}`);
 export const getNPC = id => get(`${NPCS_URL}/${id}`);
 export const putNPC = NPC => put(`${NPCS_URL}/${NPC._id}`, NPC);
@@ -91,7 +91,7 @@ export const getStory = id => get(`${STORIES_URL}/${id}`);
 export const putStory = story => put(`${STORIES_URL}/${story._id}`, story);
 export const delStory = id => del(`${STORIES_URL}/${id}`);
 
-export const postSubregion = subregion => post(`${SUBREGIONS_URL}`, subregion);
+export const postSubregion = () => post(`${SUBREGIONS_URL}`);
 export const getSubregions = () => get(`${SUBREGIONS_URL}`);
 export const getSubregion = id => get(`${SUBREGIONS_URL}/${id}`);
 export const putSubregion = subregion => put(`${SUBREGIONS_URL}/${subregion._id}`, subregion);
