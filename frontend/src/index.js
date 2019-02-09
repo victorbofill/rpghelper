c3d163424d8a2951a85c20352c33c236ace2e0be
victorbofill <victorm.bofill@gmail.com>
2019-02-08 17:33:10 -0800

    removed redux files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,9 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { Provider } from 'react-redux';
 import App from './components/app/App';
-import store from './store/store';
 import './main.css';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
+  <App/>,
   document.getElementById('root')
 );
