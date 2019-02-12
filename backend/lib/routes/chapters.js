501e7b06a2468a3fe951e55594eba9f1fbe8081a
victorbofill <victorm.bofill@gmail.com>
2019-02-11 19:15:34 -0800

    fixed chapter/story BE

--- backend/lib/routes/chapters.js
+++ backend/lib/routes/chapters.js
@@ -2,8 +2,19 @@ const router = require('express').Router();
 const { updateOptions } = require('../utils/mongoose-helpers');
 
 const Chapter = require('../models/Chapter');
+const Story = require('../models/Story');
 
 module.exports = router
+  .post('/', async(req, res, next) => {
+    const { storyId } = req.body;
+    const newChapter = await Chapter.create({});
+    await Story.findByIdAndUpdate(storyId, {
+      $addToSet: { chapters: newChapter._id }
+    }, updateOptions)
+      .catch(next);
+    return res.json(newChapter);
+  })
+
   .put('/:id', async(req, res, next) => {
     const { url, name, description, status, reward, unlocks, } = req.body;
     const update = { url, name, description, status, reward, unlocks };
@@ -14,4 +25,12 @@ module.exports = router
       .catch(next);
 
     return res.json(updatedChapter);
+  })
+
+  .delete('/:id/chapters/:chapterId', async(req, res, next) => {
+    await Chapter.findByIdAndRemove(req.params.chapterId).catch(next);
+    await Story.findByIdAndUpdate(req.params.id, {
+      $pull: { chapters: req.params.id }
+    }, updateOptions).catch(next);
+    return res.json({ deleted: true });
   });
--- backend/lib/routes/stories.js
+++ backend/lib/routes/stories.js
@@ -10,23 +10,15 @@ module.exports = router
     return res.json(story);
   })
 
-  .post('/:id/chapter', async(req, res, next) => {
-    const chapter = await Chapter.create(req.body);
-    await Story.findByIdAndUpdate(req.params.id, {
-      $addToSet: { chapters: chapter._id }
-    }, updateOptions)
-      .catch(next);
-    return res.json(chapter);
-  })
-
   .get('/', async(req, res, next) => {
-    const story = await Story.find().populate('chapters').catch(next);
+    const story = await Story.find().catch(next);
     return res.json(story);
   })
 
-  .get('/:id', async(req, res, next) => {
-    const story = await Story.findById(req.params.id).populate('chapters').catch(next);
-    return res.json(story);
+  .get('/:id/chapters', async(req, res, next) => {
+    const { chapters } = await Story.findById(req.params.id).populate('chapters').catch(next);
+    console.log('chapters: ', chapters);
+    return res.json(chapters);
   })
 
   .put('/:id', async(req, res, next) => {
@@ -43,16 +35,5 @@ module.exports = router
 
   .delete('/:id', async(req, res, next) => {
     await Story.findByIdAndRemove(req.params.id).catch(next);
-    return res.json({ deleted: true });
-  })
-
-  .delete('/:id/chapters/:chapterId', async(req, res, next) => {
-    await Chapter.findByIdAndRemove(req.params.chapterId)
-      .catch(next);
-
-    await Story.findByIdAndUpdate(req.params.id, {
-      $pull: { chapters: req.params.id }
-    }, updateOptions)
-      .catch(next);
     return res.json({ deleted: true });
   });
